f4ff27979b473c6c665774723043ee63a59c6e05
Aidan-B1409 <=>
2021-08-30 20:19:54 -0700

    Breaking change - refactored querying system

--- src/data_mining/youtube/youtubebot.py
+++ src/data_mining/youtube/youtubebot.py
@@ -1,4 +1,6 @@
 from data_mining.commentminer import CommentMiner
+from data_mining.jsonbuilder import Submission, Comment
+from typing import List
 import googleapiclient.discovery as gapiclient
 import google_auth_oauthlib.flow as gauth
 
@@ -8,6 +10,8 @@ scopes = ["https://www.googleapis.com/auth/youtube.readonly"]
 
 
 class YoutubeBot(CommentMiner):
+
+
     def __init__(self, key: str) -> None:
         yt_client = self._make_client(key)
 
@@ -16,3 +20,7 @@ class YoutubeBot(CommentMiner):
         flow = gauth.InstalledAppFlow.from_client_secrets_file(key, scopes)
         creds = flow.run_console()
         return gapiclient.build('youtube', 'v3', credentials=creds)
+
+
+    def query(self, song_name: str, artist_name: str) -> List[Submission]:
+        return list(map(self.process_submissions, self.get_submissions(song_name, artist_name)))
