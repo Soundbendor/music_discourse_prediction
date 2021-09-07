7074ac30c25c6ceec0d0fd11550fc1a792f2372c
Aidan-B1409 <=>
2021-09-06 20:45:55 -0700

    Added comment parsing and retrieval

--- src/data_mining/youtube/youtubebot.py
+++ src/data_mining/youtube/youtubebot.py
@@ -1,19 +1,11 @@
 import pyyoutube
 
 from .youtubeinterface import SubmissionInterface, YoutubeInterface
+from .youtubeinterface import lookup
 from data_mining.commentminer import CommentMiner
 from data_mining.jsonbuilder import Submission, Comment
 
 from typing import List
-
-
-
-
-
-scopes = ["https://www.googleapis.com/auth/youtube.readonly"]
-
-
-
 
 class YoutubeBot(CommentMiner):
 
@@ -39,14 +31,26 @@ class YoutubeBot(CommentMiner):
         sub_handler = SubmissionInterface(submission, self.yt_client)
         s_lang = self.l_detect(f"{sub_handler.snippet.title} {sub_handler.snippet.description}")
         return Submission(
-            title = sub_handler.lookup(sub_handler.snippet.title, str),
-            body = sub_handler.lookup(sub_handler.snippet.description, str),
+            title = lookup(sub_handler.snippet.title, str),
+            body = lookup(sub_handler.snippet.description, str),
             lang = s_lang.lang,
             lang_p = s_lang.prob,
             url = sub_handler.get_url(),
             id = sub_handler.get_video_id(),
             score = sub_handler.get_video_score(),
-            n_comments = sub_handler.lookup(sub_handler.stats.commentCount, int),
-            subreddit = sub_handler.lookup(sub_handler.snippet.channelTitle, str),
-            #comments = list(map(self.process_comments, self.get_comments(submission)))
+            n_comments = lookup(sub_handler.stats.commentCount, int),
+            subreddit = lookup(sub_handler.snippet.channelTitle, str),
+            comments = list(map(self.process_comments, self.yt_client.get_comments(sub_handler.get_video_id())))
         )
+
+    def process_comments(self, comment: pyyoutube.Comment) -> Comment:
+        c_data = lookup(comment.snippet, pyyoutube.CommentSnippet)
+        c_lang = self.l_detect(lookup(c_data.textOriginal, str))
+        return Comment(
+                id = lookup(c_data.parentId, str),
+                score = lookup(c_data.likeCount, int),
+                body = lookup(c_data.textOriginal, str),
+                replies = 0,
+                lang = c_lang.lang,
+                lang_p = c_lang.prob
+            )
--- src/data_mining/youtube/youtubeinterface.py
+++ src/data_mining/youtube/youtubeinterface.py
@@ -2,6 +2,7 @@
 import pyyoutube
 import json
 
+from itertools import chain
 from typing import List, Type
 
 from pyyoutube.api import Api
@@ -10,6 +11,12 @@ from pyyoutube.models.search_result import SearchResultId, SearchResultSnippet
 # a wrapper for a wrapper for a wrapper of the youtube API. 
 # fml. - aidan
 baseURL = 'https://youtu.be/zV9T42c1yWk'
+
+
+def lookup(field, t: Type):
+    if isinstance(field, t):
+        return field
+    raise APIResponseError()
 class YoutubeInterface:
 
     def __init__(self, key: str):
@@ -29,23 +36,34 @@ class YoutubeInterface:
             return api
 
     def search_by_keywords(self, query: str, search_type: list, count: int, limit: int) -> List[pyyoutube.SearchResult]:
-        return self.validate_return(
+        return lookup(
             self.api.search_by_keywords(q=query, search_type=search_type, count=count, limit=limit).items, 
             List[pyyoutube.SearchResult])
 
             
-    def validate_return(self, item, d_type: Type):
-        if type(item) == d_type:
-            return d_type(item)
-        raise(APIResponseError("Invalid return type for query."))
-
-
     def _get_video_responses_by_id(self, video_id) -> List[pyyoutube.Video]:
-        return self.validate_return(
+        return lookup(
             self.api.get_video_by_id(video_id=video_id).items,
             List[pyyoutube.Video]
         )
 
+    def _get_comment_threads(self, video_id) -> List[pyyoutube.CommentThread]:
+        return lookup(
+            self.api.get_comment_threads(video_id=video_id, count=None).items,
+            List[pyyoutube.CommentThread]
+        )
+    
+    def get_comments(self, video_id: str) -> List[pyyoutube.Comment]:
+        return list(chain.from_iterable(map(self._flatten_comments, self._get_comment_threads(video_id))))
+
+
+    def _flatten_comments(self, thread: pyyoutube.CommentThread) -> List[pyyoutube.Comment]:
+        snippet = lookup(thread.snippet, pyyoutube.CommentThreadSnippet)
+        replies = lookup(thread.replies, List[pyyoutube.Comment])
+        top_comment = lookup(snippet.topLevelComment, pyyoutube.Comment)
+        replies.insert(0,top_comment)
+        return replies
+            
 
     def get_video_by_id(self, video_id) -> pyyoutube.Video:
         videos = self._get_video_responses_by_id(video_id)
@@ -58,32 +76,27 @@ class SubmissionInterface:
 
     def __init__(self, submission: pyyoutube.SearchResult, api: YoutubeInterface):
         self.submission = submission
-        self.snippet = self.lookup(submission.snippet, pyyoutube.SearchResultSnippet)
+        self.snippet = lookup(submission.snippet, pyyoutube.SearchResultSnippet)
         self.video = api.get_video_by_id(self.get_video_id())
         self.stats = self._get_video_statistics()
 
 
     def get_video_id(self) -> str:
-        v_id: SearchResultId = self.lookup(self.submission.id, pyyoutube.SearchResultId)
-        return self.lookup(v_id.videoId, str)
+        v_id: SearchResultId = lookup(self.submission.id, pyyoutube.SearchResultId)
+        return lookup(v_id.videoId, str)
 
 
     def _get_video_statistics(self) -> pyyoutube.VideoStatistics:
-        return self.lookup(self.video.statistics, pyyoutube.VideoStatistics)
+        return lookup(self.video.statistics, pyyoutube.VideoStatistics)
 
 
     def get_video_score(self) -> int:
-        return self.lookup(self.stats.likeCount, int) - self.lookup(self.stats.dislikeCount, int)
+        return lookup(self.stats.likeCount, int) - lookup(self.stats.dislikeCount, int)
 
 
     def get_url(self) -> str:
         return baseURL + self.get_video_id()
 
-
-    def lookup(self, field, t: Type):
-        if isinstance(field, t):
-            return field
-        raise APIResponseError()
 
 class APIResponseError(Exception):
     pass
