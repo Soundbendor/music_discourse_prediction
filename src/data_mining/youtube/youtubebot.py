0870d47087128d1290928ff12f8b7fe9e1303040
Aidan-B1409 <=>
2021-10-07 14:44:33 -0700

    Video ID handling

--- src/data_mining/youtube/youtubebot.py
+++ src/data_mining/youtube/youtubebot.py
@@ -1,6 +1,6 @@
 from time import sleep
 from googleapiclient.errors import HttpError
-from .youtubeinterface import YoutubeInterface
+from .youtubeinterface import YoutubeInterface, YoutubeSearchResult
 from data_mining.commentminer import CommentMiner
 from data_mining.jsonbuilder import Submission, Comment
 
@@ -32,7 +32,7 @@ class YoutubeBot(CommentMiner):
                 exit()
 
 
-    def get_submissions(self, song_name: str, artist_name: str) -> List[Dict]:
+    def get_submissions(self, song_name: str, artist_name: str) -> List[YoutubeSearchResult]:
         return self.yt_client.search_by_keywords(
             query=self._build_query(song_name, artist_name), limit=self.search_depth
         )
@@ -49,24 +49,19 @@ class YoutubeBot(CommentMiner):
             return 0
 
 
-    def process_submissions(self, s_result: dict) -> Submission:
-        s_lang = self.l_detect(f"{s_result['snippet']['title']} {s_result['snippet']['description']}")
-        try:
-            v_resource = self.yt_client.get_video_by_id(s_result['id']['videoId'])[0]
-        # KeyError means no video resource found
-        except KeyError:
-            print("No results found")
+    def process_submissions(self, s_result: YoutubeSearchResult) -> Submission:
+        s_lang = self.l_detect(f"{s_result.snippet['snippet']['title']} {s_result.snippet['snippet']['description']}")
         return Submission(
-            title = s_result['snippet']['title'],
-            body = s_result['snippet']['description'],
+            title = s_result.snippet['snippet']['title'],
+            body = s_result.snippet['snippet']['description'],
             lang = s_lang.lang,
             lang_p = s_lang.prob,
-            url = base_url + s_result['id']['videoId'],
-            id = s_result['id']['videoId'],
-            score = self._get_video_score(v_resource),
-            n_comments = v_resource['statistics']['commentCount'],
-            subreddit = v_resource['snippet']['channelTitle'],
-            comments = list(map(self.process_comments, self.yt_client.get_comments(s_result['id']['videoId'])))
+            url = base_url + s_result.snippet['id']['videoId'],
+            id = s_result.snippet['id']['videoId'],
+            score = self._get_video_score(s_result.video),
+            n_comments = s_result.video['statistics']['commentCount'],
+            subreddit = s_result.video['snippet']['channelTitle'],
+            comments = list(map(self.process_comments, self.yt_client.get_comments(s_result.snippet['id']['videoId'])))
         )
 
 
