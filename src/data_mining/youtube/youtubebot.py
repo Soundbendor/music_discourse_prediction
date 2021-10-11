d31c269dc4ed639d1e9ac1c790c0bfb51ef250c7
Aidan-B1409 <=>
2021-10-10 21:12:20 -0700

    Fixing placement of api quota sleep loop

--- src/data_mining/youtube/youtubebot.py
+++ src/data_mining/youtube/youtubebot.py
@@ -17,9 +17,16 @@ class YoutubeBot(CommentMiner):
 
 
     def query(self, song_name: str, artist_name: str) -> List[Submission]:
+        return list(map(self.process_submissions, self.get_submissions(song_name, artist_name)))
+
+
+
+    def get_submissions(self, song_name: str, artist_name: str) -> List[YoutubeSearchResult]:
         for i in range(0,3):
             try:
-                return list(map(self.process_submissions, self.get_submissions(song_name, artist_name)))
+                return self.yt_client.search_by_keywords(
+                    query=self._build_query(song_name, artist_name), limit=self.search_depth
+                )
             except HttpError as e:
                 print(e)
                 print(self._build_query(song_name, artist_name))
@@ -28,18 +35,12 @@ class YoutubeBot(CommentMiner):
                 continue
 
 
-    def get_submissions(self, song_name: str, artist_name: str) -> List[YoutubeSearchResult]:
-        return self.yt_client.search_by_keywords(
-            query=self._build_query(song_name, artist_name), limit=self.search_depth
-        )
-
-
     def _build_query(self, song_name: str, artist_name: str) -> str:
         return f"\"{artist_name}\" \"{song_name}\""
 
     def _get_comment_count(self, v_resource: Dict) -> int:
         try: 
-            int(v_resource['statistics']['commentCount'])
+            return int(v_resource['statistics']['commentCount'])
         except KeyError:
             return 0
 
@@ -52,18 +53,26 @@ class YoutubeBot(CommentMiner):
 
     def process_submissions(self, s_result: YoutubeSearchResult) -> Submission:
         s_lang = self.l_detect(f"{s_result.snippet['snippet']['title']} {s_result.snippet['snippet']['description']}")
-        return Submission(
-            title = s_result.snippet['snippet']['title'],
-            body = s_result.snippet['snippet']['description'],
-            lang = s_lang.lang,
-            lang_p = s_lang.prob,
-            url = base_url + s_result.snippet['id']['videoId'],
-            id = s_result.snippet['id']['videoId'],
-            score = self._get_video_score(s_result.video),
-            n_comments = self._get_comment_count(s_result.video),
-            subreddit = s_result.video['snippet']['channelTitle'],
-            comments = list(map(self.process_comments, self.yt_client.get_comments(s_result.snippet['id']['videoId'])))
-        )
+        for i in range(0,3):
+            try:
+                return Submission(
+                    title = s_result.snippet['snippet']['title'],
+                    body = s_result.snippet['snippet']['description'],
+                    lang = s_lang.lang,
+                    lang_p = s_lang.prob,
+                    url = base_url + s_result.snippet['id']['videoId'],
+                    id = s_result.snippet['id']['videoId'],
+                    score = self._get_video_score(s_result.video),
+                    n_comments = self._get_comment_count(s_result.video),
+                    subreddit = s_result.video['snippet']['channelTitle'],
+                    comments = list(map(self.process_comments, self.yt_client.get_comments(s_result.snippet['id']['videoId'])))
+                )
+            except HttpError as e:
+                print(e)
+                print(s_result.snippet['snippet']['title'])
+                print("Entering 24hr sleep loop")
+                sleep(86400)
+                continue
 
 
     def process_comments(self, comment: dict) -> Comment:
