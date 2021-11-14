c6938b1bed61ff8764d49d33ce5ea260d7daa73c
Aidan-B1409 <=>
2021-11-14 15:23:43 -0800

    Moved rate limiter and increased wait time

--- src/data_mining/twitter/twitterbot.py
+++ src/data_mining/twitter/twitterbot.py
@@ -27,13 +27,14 @@ class TwitterBot(CommentMiner):
 
     def get_submissions(self, song_name: str, artist_name: str) -> Iterator[tweepy.Tweet]:
         # returns a list of top-level tweets mentioning the artist/track title
+        # avoid the rate limit. 1 tweet/sec limit on historical queries. 
+        sleep(15)
         return tweepy.Paginator(self.api.search_all_tweets,
                                 self._build_query(song_name, artist_name),
                                 max_results=500,
                                 expansions='referenced_tweets.id,author_id,in_reply_to_user_id',
                                 tweet_fields='entities,geo,lang,public_metrics,conversation_id,created_at',
                                 user_fields='username').flatten()
-
 
     def _lookup_user(self, user_id) -> tweepy.Response:
         response = self.api.get_user(id = user_id)
@@ -45,8 +46,7 @@ class TwitterBot(CommentMiner):
     def process_submissions(self, p_tweet: tweepy.Tweet) -> Submission:
         s_lang = self.l_detect(p_tweet.text)
         user = self._lookup_user(p_tweet.author_id).data
-        # avoid the rate limit. 1 tweet/sec limit on historical queries. 
-        sleep(0.5)
+
         return Submission(
             title = "",
             body = p_tweet.text,
