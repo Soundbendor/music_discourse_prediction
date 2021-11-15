42ce6f48f49392e019f5d51c4341631274a536f1
Aidan-B1409 <=>
2021-11-14 18:23:04 -0800

    Removed rate limiter?

--- src/feature_engineering/featureengineering.py
+++ src/feature_engineering/featureengineering.py
@@ -1,8 +1,16 @@
 import argparse
+import pandas as pd
+import cudf
 
+
+# Plans to support:
+# - nltk Vader sentiment feature generation
+# - "traditional" affect/VAD analysis features via cuDF
+# - BERT classification via Tensorflow
 
 def main():
     args = parseargs()
+    
 
 def parseargs() -> argparse.Namespace:
     parser = argparse.ArgumentParser(
@@ -13,4 +21,7 @@ def parseargs() -> argparse.Namespace:
         help = 'Path to the directory containing the wordlists necesscary for feature generation.')
     parser.add_argument('-p', '--processes', type=int, default='16', dest='processes',
         help = 'Number of processes to spawn')
+    parser.add_argument('--source', required=True, type=str, dest='sm_type', 
+        help = "Which type of social media input is being delivered.\n\
+            Valid options are [Twitter, Youtube, Reddit, Lyrics].")
     return parser.parse_args()
