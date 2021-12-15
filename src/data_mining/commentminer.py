b3fe2364a6b5035c61afdcaaade3cb0a2e1c6b8d
Aidan-B1409 <=>
2021-12-14 17:20:02 -0800

    Updated quote removal in query building

--- src/data_mining/commentminer.py
+++ src/data_mining/commentminer.py
@@ -27,7 +27,9 @@ class CommentMiner:
         return list(map(self.process_submissions, self.get_submissions(song_name, artist_name)))
     
     def _build_query(self, song_name: str, artist_name: str) -> str:
-        return f'\"{artist_name.replace('"', '')}\" \"{song_name.replace('"', '')}\"'
+        song_name = song_name.replace('"', '')
+        artist_name = artist_name.replace('"', '')
+        return f'\"{artist_name}\" \"{song_name}\"'
 
     @abstractmethod
     def get_submissions(self, song_name: str, artist_name: str) -> Iterator:
