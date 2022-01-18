286f71d1616d050e64848bdcfcfd564ddb16a91b
Aidan-B1409 <=>
2022-01-18 14:38:21 -0800

    Staging

--- src/feature_engineering/bert/bert_emotion_predictor.py
+++ src/feature_engineering/bert/bert_emotion_predictor.py
@@ -1,1 +1,5 @@
-from transformers import 
+from transformers.models.distilbert.tokenization_distilbert import DistilBertTokenizer
+
+distil_bert = 'distilbert-base-uncased-emotion'
+
+tokenizer = DistilBertTokenizer.from_pretrained(distil_bert, do_lower_case=True, add_special_tokens=True)
