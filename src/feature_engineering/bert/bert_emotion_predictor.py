ad7bfbac03d346c131d20a38dbfacf23db6cd7ba
Aidan-B1409 <=>
2022-02-03 14:33:01 -0800

    use our own prediction head

--- src/feature_engineering/bert/bert_emotion_predictor.py
+++ src/feature_engineering/bert/bert_emotion_predictor.py
@@ -8,7 +8,7 @@ import transformers
 from typing import Tuple
 from tqdm import tqdm
 from transformers import DistilBertTokenizer
-from transformers import TFDistilBertForSequenceClassification
+from transformers import TFDistilBertModel
 from transformers import DistilBertConfig
 
 from feature_engineering.song_loader import get_song_df
@@ -73,31 +73,30 @@ def main():
     # TODO - need a train-test split
     song_embeddings = generate_embeddings(song_df, tokenizer)
 
-    # 2 labels: valence, arousal
-    config = DistilBertConfig(num_labels=NUM_LABEL)
+
+    config = DistilBertConfig(dropout=0.2, attention_dropout=0.2)
     config.output_hidden_states = False
-
-
-    transformer_model = TFDistilBertForSequenceClassification.from_pretrained(distil_bert, config = config)
+    transformer_model = TFDistilBertModel.from_pretrained(distil_bert, config = config)
     
     input_ids = tf.keras.layers.Input(shape=(MAX_SEQ_LEN,), name='input_token', dtype='int32')
     input_masks_ids = tf.keras.layers.Input(shape=(MAX_SEQ_LEN,), name='masked_token', dtype='int32')
 
-    output = transformer_model(input_ids, input_masks_ids)[0]
+    embed_layer = transformer_model(input_ids, input_masks_ids)[0]
+    output = tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(50, return_sequences=True, dropout=0.1, recurrent_dropout=0.1))(embed_layer)
+    output = tf.keras.layers.GlobalAveragePooling1D()(output)
+    output = tf.keras.layers.Dense(50, activation='relu')(output)
+    output = tf.keras.layers.Dropout(0.2)(output)
     output = tf.keras.layers.Dense(NUM_LABEL, activation='relu')(output)
     model = tf.keras.Model(inputs=[input_ids, input_masks_ids], outputs = output)
 
     opt = tf.keras.optimizers.Adam(learning_rate=5e-5)
 
-    # TODO - use a better metric, idiot
-    # NOTE - this is where the last error hits - dimension issue
     model.compile(optimizer=opt, loss='mse', metrics=tf.keras.metrics.RootMeanSquaredError())
 
-    model.get_layer(index=2).trainable = False
-    print(model.summary())
-    print(song_embeddings)
+    model.get_layer(index=3).trainable = False
 
-    model.fit(song_embeddings)
+
+    model.fit(song_embeddings, verbose=1, epochs=100)
 
     # logits = model.predict([song_df['input_ids'].to_numpy(), song_df['input_masks'].to_numpy()], verbose=1).logits
 
