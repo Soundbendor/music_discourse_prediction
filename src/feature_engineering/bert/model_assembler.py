aca443e77ccb1d984c4eebdb243e731e9c8dff49
Aidan-B1409 <=>
2022-02-09 21:16:48 -0800

    changed number of targets for bert

--- src/feature_engineering/bert/model_assembler.py
+++ src/feature_engineering/bert/model_assembler.py
@@ -42,15 +42,15 @@ def create_model() -> tf.keras.Model:
     return model
 
 def create_direct_model() -> tf.keras.Model:
-    config = DistilBertConfig(num_labels=1)
+    config = DistilBertConfig(num_labels=NUM_LABEL)
     db_seq = TFDistilBertForSequenceClassification.from_pretrained(distil_bert, config=config)
 
     input_ids = tf.keras.layers.Input(shape=(MAX_SEQ_LEN,), name='input_token', dtype='int32')
     input_masks_ids = tf.keras.layers.Input(shape=(MAX_SEQ_LEN,), name='masked_token', dtype='int32')
 
     output = db_seq(input_ids, input_masks_ids)[0]
-    output = tf.keras.layers.Dropout(0.2)(output)
-    output = tf.keras.layers.Dense(2, activation='relu')(output)
+    # output = tf.keras.layers.Dropout(0.2)(output)
+    # output = tf.keras.layers.Dense(2, activation='relu')(output)
 
     model = tf.keras.Model(inputs=[input_ids, input_masks_ids], outputs = output)
     opt = tf.keras.optimizers.Adam(learning_rate=5e-5)
