8dc2223c4bff29216d6a66c1a68327e33bc6dd91
Aidan-B1409 <aidanb1049@gmail.com>
2022-03-12 18:14:40 -0800

    Fixed dataset sharding - should fix divide-by-zero error in validation loop

--- src/feature_engineering/bert/discourse_dataset.py
+++ src/feature_engineering/bert/discourse_dataset.py
@@ -18,39 +18,37 @@ class DiscourseDataSet:
         self.batch_size = batch_size
         self.options = options
         self.train, self.test, self.validate = self._to_datasets(df)
-        
 
     def _to_datasets(self, df: pd.DataFrame) -> Tuple[TFDataset, TFDataset, TFDataset]:
         ds = self._generate_embeddings(df)
-        ds_train = ds.skip(int(2*(df.shape[1] * self.test_prop)))
+        ds_train = ds.skip(int(2*(df.shape[0] * self.test_prop)))
         # we derive validation and test from holdout - this is a temp variable
-        ds_holdout = ds.take(int(2*(df.shape[1] * self.test_prop)))
+        ds_holdout = ds.take(int(2*(df.shape[0] * self.test_prop)))
         ds_test, ds_validate = ds_holdout.shard(2, 0), ds_holdout.shard(2, 1)
         # IMPORTANT: MUST drop remainder in order to prevent segfault when training with multiple GPUs + cuDNN kernel function
         return tuple(map(lambda x: x.batch(self.batch_size, drop_remainder=True).with_options(self.options), [ds_train, ds_test, ds_validate]))
 
-
     def _tokenize(self, comments: pd.Series, tokenizer) -> transformers.BatchEncoding:
         return tokenizer(list(comments),
-            add_special_tokens = True,
-            return_attention_mask = True,
-            return_token_type_ids = False,
-            max_length = self.seq_len,
-            padding = 'max_length',
-            truncation = True,
-            return_tensors = 'tf')
+                         add_special_tokens=True,
+                         return_attention_mask=True,
+                         return_token_type_ids=False,
+                         max_length=self.seq_len,
+                         padding='max_length',
+                         truncation=True,
+                         return_tensors='tf')
 
     def _generate_embeddings(self, df: pd.DataFrame) -> tf.data.Dataset:
         tokenizer = DistilBertTokenizer.from_pretrained(distil_bert,
-            do_lower_case=True,
-            add_special_tokens=True,
-            max_length=self.seq_len,
-            padding='max_length',
-            truncate=True,
-            padding_side='right')
+                                                        do_lower_case=True,
+                                                        add_special_tokens=True,
+                                                        max_length=self.seq_len,
+                                                        padding='max_length',
+                                                        truncate=True,
+                                                        padding_side='right')
 
         encodings = self._tokenize(df['body'], tokenizer)
         return tf.data.Dataset.from_tensor_slices(({
             'input_token': encodings['input_ids'],
             'masked_token': encodings['attention_mask']},
-            tf.constant((df[['valence', 'arousal']].values).astype('float32')))) 
+            tf.constant((df[['valence', 'arousal']].values).astype('float32'))))
