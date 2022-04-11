d1c46e75ed59b430a6df5bfdc754601345c5ab78
Aidan-B1409 <aidanb1049@gmail.com>
2022-04-10 17:08:31 -0700

    fixed train test splitting

--- src/feature_engineering/wordbag_model/wordbag.py
+++ src/feature_engineering/wordbag_model/wordbag.py
@@ -24,42 +24,43 @@ def parseargs() -> argparse.Namespace:
     parser = argparse.ArgumentParser(
         description="Feature extraction toolkit for music semantic analysis from various social media platforms.")
     parser.add_argument('-i', '--input_dir', dest='input', type=str,
-        help = "Path to the directory storing the JSON files for social media data.")
-    parser.add_argument('-w', '--wordlist', dest='wordlist', type=str, 
-        help = 'Wordlist to generate features from. Valid options are: [EmoVAD, EmoAff, EmoLex, eANEW, MPQA]')
-    parser.add_argument('--source', required=True, type=str, dest='sm_type', 
-        help = "Which type of social media input is being delivered.\n\
+                        help="Path to the directory storing the JSON files for social media data.")
+    parser.add_argument('-w', '--wordlist', dest='wordlist', type=str,
+                        help='Wordlist to generate features from. Valid options are: [EmoVAD, EmoAff, EmoLex, eANEW, MPQA]')
+    parser.add_argument('--source', required=True, type=str, dest='sm_type',
+                        help="Which type of social media input is being delivered.\n\
             Valid options are [Twitter, Youtube, Reddit, Lyrics].")
     parser.add_argument('--dataset', type=str, dest='dataset', required=True,
-        help = "Name of the dataset which the comments represent")
+                        help="Name of the dataset which the comments represent")
     return parser.parse_args()
 
 
 def _tokenize_comment(comment: str):
     rx = re.compile(r'(?:<.*?>)|(?:[^\w\s\'])|(?:\d+)')
     lemmatizer = WordNetLemmatizer()
-    stop_words = stopwords.words('english')  
+    stop_words = stopwords.words('english')
 
     return pd.Series(
-            filter(lambda x: x not in stop_words,
-                map(lemmatizer.lemmatize,
-                    nltk.word_tokenize(
-                        rx.sub('', comment)
-                    )
-                )
-            )
-        ).value_counts().reset_index().rename(columns={'index': 'Word', 0: 'Count'})
+        filter(lambda x: x not in stop_words,
+               map(lemmatizer.lemmatize,
+                   nltk.word_tokenize(
+                       rx.sub('', comment)
+                   )
+                   )
+               )
+    ).value_counts().reset_index().rename(columns={'index': 'Word', 0: 'Count'})
 
 
 def vectorize_comment(x: pd.Series, wordlist: pd.DataFrame):
     c_vec = (pd.concat(list(x), axis=0, ignore_index=True)
-            .pipe(pd.merge, wordlist, on='Word')
-            .drop(['Word', 'Count'], axis=1)
-            .aggregate(['min', 'max', 'mean', 'std'])
-            .stack()
-        )
+             .pipe(pd.merge, wordlist, on='Word')
+             .drop(['Word', 'Count'], axis=1)
+             .aggregate(['min', 'max', 'mean', 'std'])
+             .stack()
+             )
 
-    c_vec.index = pd.Index(map(lambda x: f"{x[0]}.{x[1]}", c_vec.index.to_flat_index()))
+    c_vec.index = pd.Index(
+        map(lambda x: f"{x[0]}.{x[1]}", c_vec.index.to_flat_index()))
     return c_vec.to_frame().T
 
 
@@ -72,20 +73,22 @@ def tokenize_comments(df: pd.DataFrame):
 # |--------|-----------|----------------|
 # | Damage |   anger   |       1        |
 # |        |    joy    |       0        |
-#........................................
+# ........................................
 # |  Word  |   Anger    |   Joy   |
 # |--------|------------|---------|
 # | Damage |     1      |    0    |
 
+
 def load_emolex(path: str) -> pd.DataFrame:
-    return (pd.read_csv(path, names=['Word','Emotion','Association'], skiprows=1, sep='\t')
-        .groupby(['Word']).apply(lambda x: x.set_index('Emotion')['Association'])
-        .reset_index()
-    )
+    return (pd.read_csv(path, names=['Word', 'Emotion', 'Association'], skiprows=1, sep='\t')
+            .groupby(['Word']).apply(lambda x: x.set_index('Emotion')['Association'])
+            .reset_index()
+            )
 
 
 def check_affect(sub_df: pd.DataFrame, key: str):
-    affects = sub_df['Affect'].reset_index(drop=True).str.contains(key, regex=False)
+    affects = sub_df['Affect'].reset_index(
+        drop=True).str.contains(key, regex=False)
     affects = affects[affects]
     if affects.index.empty:
         return 0
@@ -101,22 +104,26 @@ def get_scores(sub_df: pd.DataFrame, df2: pd.DataFrame):
         'Fear': check_affect(sub_df, 'fear')}, index=[0])
 
 # Unlike Emolex, each subgroup contains a variable number of affects (0-6)
+
+
 def load_emoaff(path: str) -> pd.DataFrame:
-    df = pd.read_csv(path, names=['Word','Score','Affect'], skiprows=1, sep='\t')
+    df = pd.read_csv(path, names=['Word', 'Score',
+                     'Affect'], skiprows=1, sep='\t')
     df2 = pd.DataFrame(columns=['Word', 'Anger', 'Joy', 'Sadness', 'Fear'])
     rows = df.groupby('Word').apply(lambda x: get_scores(x, df2))
     return df2.append(rows, ignore_index=True)
 
 
 def load_mpqa(path: str) -> pd.DataFrame:
-    return(pd.read_csv(path,  names=['Word','Sentiment'], skiprows=0)
-            .drop_duplicates(subset='Word')
-            .replace({'Sentiment': {'positive': 1, 'negative': -1, 'neutral': 0, 'both': 0}}))
+    return(pd.read_csv(path,  names=['Word', 'Sentiment'], skiprows=0)
+           .drop_duplicates(subset='Word')
+           .replace({'Sentiment': {'positive': 1, 'negative': -1, 'neutral': 0, 'both': 0}}))
+
 
 loaders = {
     "eANEW": lambda x: pd.read_csv(x, encoding='utf-8', engine='python', index_col=0),
     "EmoLex": load_emolex,
-    "EmoVAD": lambda x: pd.read_csv(x, names=['Word','Valence','Arousal','Dominance'], skiprows=1,  sep='\t'),
+    "EmoVAD": lambda x: pd.read_csv(x, names=['Word', 'Valence', 'Arousal', 'Dominance'], skiprows=1,  sep='\t'),
     "EmoAff": load_emoaff,
     "HSsent": None,
     "MPQA": load_mpqa,
@@ -147,15 +154,16 @@ def gen_features(wlist, args):
     fname = f"{args.dataset}_{args.sm_type}_{timestamp}_{wlist}_features.csv"
 
     uncompressible_cols = ['submission.subreddit', 'submission.id', 'submission.url', 'submission.lang',
-                'submission.lang_p', 'id', 'lang', 'lang_p', 'replies',
-                'submission.title', 'submission.body']
+                           'submission.lang_p', 'id', 'lang', 'lang_p', 'replies',
+                           'submission.title', 'submission.body']
 
     # TODO - Handle submission titles, submission bodies, WITHOUT dropping them. Tokenize and emovectorize.
     df = (get_song_df(args.input)
-            .pipe(tokenize_comments)
-            .drop(uncompressible_cols, axis=1))
+          .pipe(tokenize_comments)
+          .drop(uncompressible_cols, axis=1))
 
-    emo_word_stats = df.groupby(['query_index'])['body'].apply(lambda x: vectorize_comment(x, wordlist))
+    emo_word_stats = df.groupby(['query_index'])['body'].apply(
+        lambda x: vectorize_comment(x, wordlist))
 
     df.drop('body', axis=1, inplace=True)
 
@@ -166,10 +174,9 @@ def gen_features(wlist, args):
         'arousal': lambda x: x.iloc[0],
         'valence': lambda x: x.iloc[0],
         'dataset': lambda x: x.iloc[0],
-        'artist_name': lambda x: x.iloc[0], 
+        'artist_name': lambda x: x.iloc[0],
         'song_name': lambda x: x.iloc[0],
     })
-
 
     df3 = df.join(emo_word_stats)
     df3.to_csv(fname)
