3e732dcfd79f7a52c70171eca7db74d4571eccc8
Aidan-B1409 <aidanb1049@gmail.com>
2022-04-15 11:16:36 -0700

    validation

--- src/feature_engineering/bert/discourse_dataset.py
+++ src/feature_engineering/bert/discourse_dataset.py
@@ -39,8 +39,8 @@ class DiscourseDataSet:
     def __init__(self, df: pd.DataFrame, t_prop: float):
         self.df = self._clean_str(df)
         # TODO - introduce validation subset
-        self.X_train, self.X_test, self.y_train, self.y_test = self._split_data(self.df,
-                                                                                test_size=t_prop)
+        self.X_train, self.X_val, self.X_test, self.y_train, self.y_val, self.y_test = self._split_data(self.df,
+                                                                                                        test_size=t_prop)
 
     # NOTE - ONLY cleans comment bodies. Adapt to post titles if needed.
     def _clean_str(self, df: pd.DataFrame):
@@ -48,17 +48,32 @@ class DiscourseDataSet:
         df['body'] = df['body'].apply(lambda x: rx.sub('', x))
         return df
 
+
     def _split_data(self, df: pd.DataFrame, test_size):
         np.random.seed(RAND_SEED)
         ids = df['song_name'].unique()
-        test_indices = np.random.choice(ids, size=int(len(ids) * test_size), replace=False)
+        holdout_indices = np.random.choice(ids, size=int(len(ids) * test_size), replace=False)
+        train_subset = df.loc[~df['song_name'].isin(holdout_indices)]
+        test_indices = np.random.choice(holdout_indices, size=int(len(holdout_indices) * 0.5), replace=False)
         test_subset = df.loc[df['song_name'].isin(test_indices)]
-        train_subset = df.loc[~df['song_name'].isin(test_indices)]
+        validation_subset = df.loc[~df['song_name'].isin(test_indices)]
         print(test_subset.shape)
         print(train_subset.shape)
-        return train_subset.drop(['valence', 'arousal'], axis=1), test_subset.drop(['valence', 'arousal'], axis=1), self._convert_labels(train_subset[['valence', 'arousal']]), self._convert_labels(test_subset[['valence', 'arousal']])
+        print(validation_subset.shape)
+        return self._features(train_subset),\
+            self._features(validation_subset),\
+            self._features(test_subset),\
+            self._convert_labels(train_subset),\
+            self._convert_labels(validation_subset),\
+            self._convert_labels(test_subset)
 
-    def _convert_labels(self, a):
+
+    def _features(self, a: pd.DataFrame) -> pd.DataFrame:
+        return a.drop(['valence', 'arousal'], axis=1)
+
+
+    def _convert_labels(self, a: pd.DataFrame):
+        a = a[['valence', 'arousal']]
         scaler = MinMaxScaler(feature_range=(0,1))
         a = scaler.fit_transform(a)
         return np.asarray(a).astype('float32')
