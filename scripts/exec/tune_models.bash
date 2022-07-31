ad20c2c1a8d210026fbb3d884eb1129c0f524e4e
Aidan-B1409 <aidanb1049@gmail.com>
2022-07-31 16:46:23 -0700

    fix model tuning script

--- scripts/exec/tune_models.bash
+++ scripts/exec/tune_models.bash
@@ -1,3 +1,3 @@
 for model in ada knn lightgbm mlp rf svm; do
-    prediction -c "etc/experiments/regression_experiment_${model}.ini" "etc/features/moaf/AMG1608_MOAF_${model}.csv" -o "AMG1608_MOAF_${model}" 
+    prediction -c "etc/experiments/regression_experiment_${model}.ini" "etc/features/moaf/AMG1608_MOAF_${model}.csv" -o "AMG1608_MOAF.csv" 
 done
