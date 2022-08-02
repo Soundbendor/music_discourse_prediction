3b841956c41b33bd230f50b49aa835cb5f94675f
Aidan-B1409 <aidanb1049@gmail.com>
2022-08-01 20:59:29 -0700

    fix for objects in parameter grid outputs

--- scripts/analysis/ratio_stats.py
+++ scripts/analysis/ratio_stats.py
@@ -22,24 +22,25 @@ df_ratio_results = pd.DataFrame()
 for ds in datasets.keys():
     for src in datasets[ds]:
         df = get_song_df(os.path.join(path, ds, src))
-        print(df)
         print(f"Stats for {src} in {ds}\n\n")
+        print(len(df.dropna(subset=['body'])['query_index'].unique()))
 
         df_total_results.loc[f'{ds}_{src}', 'Total Songs'] = len(
             df['query_index'].unique())
+        df_total_results.loc[f'{ds}_{src}', 'Total Songs  w/ comments'] = len(
+            df.dropna(subset=['body'])['query_index'].unique())
         df_total_results.loc[f'{ds}_{src}', 'Total Submissions'] = len(
             df['submission.id'].unique())
         df_total_results.loc[f'{ds}_{src}', 'Total Comments'] = len(df)
 
-        df[['query_index', 'body']].groupby(
-            'query_index').agg({'body': lambda x: sum(x.apply(lambda y: print(y)))})
+        # df[['query_index', 'body']].groupby('query_index').agg({'body': lambda x: x.apply(lambda y: print(y.split()))})
 
         df_ratio_results.loc[f'{ds}_{src}', 'Submissions per Song'] = df[[
             'query_index', 'submission.id']].groupby('query_index').agg('nunique').mean()['submission.id']
         df_ratio_results.loc[f'{ds}_{src}', 'Comments per Song'] = df[[
             'query_index', 'submission.id']].groupby('query_index').agg('count').mean()['submission.id']
         df_ratio_results.loc[f'{ds}_{src}', 'Words per Song'] = df[['query_index', 'body']].groupby(
-            'query_index').agg({'body': lambda x: sum(x.apply(lambda y: len(y.split())))}).mean()['body']
+            'query_index').agg({'body': lambda x: sum(x.apply(lambda y: len(str(y).split())))}).mean()['body']
         df_ratio_results.loc[f'{ds}_{src}', 'Comments per Submission'] = df[[
             'submission.id', 'body']].groupby('submission.id').agg('count').mean()['body']
         df_ratio_results.loc[f'{ds}_{src}', 'Words per Submission'] = df[['submission.id', 'body']].groupby(
@@ -50,9 +51,9 @@ for ds in datasets.keys():
         df_avg_results.loc[f'{ds}_{src}', 'Comment Std.'] = df[[
             'query_index', 'body']].groupby('query_index').agg('count').std()['body']
         df_avg_results.loc[f'{ds}_{src}', 'Word Avg.'] = sum(
-            [len(x.split()) for x in df['body']]) / len(df['query_index'].unique())
+            [len(str(x).split()) for x in df['body']]) / len(df['query_index'].unique())
         df_avg_results.loc[f'{ds}_{src}', 'Word Std.'] = np.std(
-            [len(x.split()) for x in df['body']])
+            [len(str(x).split()) for x in df['body']])
 
 print(df_total_results)
 print(df_ratio_results)
