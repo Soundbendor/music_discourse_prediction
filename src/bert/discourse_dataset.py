d70d5c7367a65f5601d01f7ba14e99613d13cc20
Aidan-B1409 <aidanb1049@gmail.com>
2022-08-07 16:05:00 -0700

    Fixed tokenizers

--- src/bert/discourse_dataset.py
+++ src/bert/discourse_dataset.py
@@ -24,20 +24,20 @@ def _tokenize(comments: pd.Series, tokenizer, seq_len: int) -> transformers.Batc
 
 def generate_embeddings(df: pd.DataFrame, seq_len: int, model: str) -> dict:
     tokenizers = {
-        'distilbert-base-cased': DistilBertTokenizerFast('distilbert-base-uncased',
-                                                         do_lower_case=True,
-                                                         add_special_tokens=True,
-                                                         max_length=seq_len,
-                                                         padding='max_length',
-                                                         truncate=True,
-                                                         padding_side='right'),
-        'roberta-base': RobertaTokenizerFast('roberta-based',
-                                             do_lower_case=True,
-                                             add_special_tokens=True,
-                                             max_length=seq_len,
-                                             padding='max_length',
-                                             truncate=True,
-                                             padding_side='right')
+        'distilbert-base-cased': DistilBertTokenizerFast.from_pretrained('distilbert-base-uncased',
+                                                                         do_lower_case=True,
+                                                                         add_special_tokens=True,
+                                                                         max_length=seq_len,
+                                                                         padding='max_length',
+                                                                         truncate=True,
+                                                                         padding_side='right'),
+        'roberta-base': RobertaTokenizerFast.from_pretrained('roberta-base',
+                                                             do_lower_case=True,
+                                                             add_special_tokens=True,
+                                                             max_length=seq_len,
+                                                             padding='max_length',
+                                                             truncate=True,
+                                                             padding_side='right')
     }
 
     tokenizer = tokenizers[model]
