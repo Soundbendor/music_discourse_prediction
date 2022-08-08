a219bf6b75546a811ca774eb22d9e18961c149a3
Aidan-B1409 <aidanb1049@gmail.com>
2022-08-07 20:33:44 -0700

    switch to distilbert uncased

--- src/bert/discourse_dataset.py
+++ src/bert/discourse_dataset.py
@@ -24,13 +24,13 @@ def _tokenize(comments: pd.Series, tokenizer, seq_len: int) -> transformers.Batc
 
 def generate_embeddings(df: pd.DataFrame, seq_len: int, model: str) -> dict:
     tokenizers = {
-        'distilbert-base-cased': DistilBertTokenizerFast.from_pretrained('distilbert-base-uncased',
-                                                                         do_lower_case=True,
-                                                                         add_special_tokens=True,
-                                                                         max_length=seq_len,
-                                                                         padding='max_length',
-                                                                         truncate=True,
-                                                                         padding_side='right'),
+        'distilbert-base-uncased': DistilBertTokenizerFast.from_pretrained('distilbert-base-uncased',
+                                                                           do_lower_case=True,
+                                                                           add_special_tokens=True,
+                                                                           max_length=seq_len,
+                                                                           padding='max_length',
+                                                                           truncate=True,
+                                                                           padding_side='right'),
         'roberta-base': RobertaTokenizerFast.from_pretrained('roberta-base',
                                                              do_lower_case=True,
                                                              add_special_tokens=True,
