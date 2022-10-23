5d1ac56c6ac6d2bbec3cd1a355c23c2cb57ed702
Aidan-B1409 <aidanb1049@gmail.com>
2022-10-22 20:04:32 -0700

    Add mongodb song collection script

--- mongotest.py
+++ mongotest.py
@@ -3,4 +3,16 @@ from pymongo import MongoClient
 client = MongoClient()
 
 db = client['test']
-print(db)
+songs = db["songs"]
+
+#  songs.insert_one({
+    #  "artist_name": "Madeon",
+    #  "track_title": "Pop Culture"
+    #  })
+#  print(db)
+
+item_details = songs.find()
+for item in item_details:
+    print(item)
+
+
