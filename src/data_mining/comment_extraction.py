ae1a9bac087308cc414c444d47d9c0190396d86b
Aidan-B1409 <aidanb1049@gmail.com>
2022-10-29 20:07:19 -0700

    Update insert logic for Deezer, now all deezer songs belong to same dataset

--- src/data_mining/comment_extraction.py
+++ src/data_mining/comment_extraction.py
@@ -12,6 +12,7 @@ from .lyrics.geniusbot import GeniusBot
 from .twitter.twitterbot import TwitterBot
 from datetime import datetime
 from tqdm import tqdm
+from pymongo import MongoClient
 
 # We assume all input datasets have a standardized input API
 # song_id, valence, arousal, song_name, artist_name
@@ -20,30 +21,34 @@ from tqdm import tqdm
 
 def main():
     args = parseargs()
-    bot = args.bot_type(args.config)
-    dataset = pd.read_csv(args.input)
-    path = f"{args.output}/downloads/"
-    dispatch_queries(bot, dataset, path, args.dataset)
+    bot = args.bot_type()
+    db_client = MongoClient()['mdp']
+    songs = db_client['songs'].find()
+    for song in songs:
+        print(song)
+    #  songs.update_many({}, )
+
+        
 
 
-def dispatch_queries(miner: CommentMiner, df, path: str, ds_name: str):
-    os.makedirs(os.path.dirname(path), exist_ok=True)
-    print(f"Beginning mining comments from {miner.__class__.__name__}...")
-
-    for idx, row in tqdm(df.iterrows(), total=len(df)):
-        dtime = datetime.now().strftime('%d-%m-%Y-%H-%M-%S')
-        fname = f"{path}{miner.__class__.__name__}_{dtime}_{row['song_id']}.json"
-        with open(fname, 'w') as out:
-            json.dump(dataclasses.asdict(SearchResult(
-                song_name=row['song_name'],
-                artist_name=row['artist_name'],
-                query_index=idx,
-                dataset=ds_name,
-                valence=row['valence'],
-                arousal=row['arousal'],
-                submissions=miner.query(row['song_name'], row['artist_name']))),
-                out, indent=4, ensure_ascii=False)
-
+#  def dispatch_queries(miner: CommentMiner, df, path: str, ds_name: str):
+    #  os.makedirs(os.path.dirname(path), exist_ok=True)
+    #  print(f"Beginning mining comments from {miner.__class__.__name__}...")
+    #
+    #  for idx, row in tqdm(df.iterrows(), total=len(df)):
+    #      dtime = datetime.now().strftime('%d-%m-%Y-%H-%M-%S')
+    #      fname = f"{path}{miner.__class__.__name__}_{dtime}_{row['song_id']}.json"
+    #      with open(fname, 'w') as out:
+    #          json.dump(dataclasses.asdict(SearchResult(
+    #              song_name=row['song_name'],
+    #              artist_name=row['artist_name'],
+    #              query_index=idx,
+    #              dataset=ds_name,
+    #              valence=row['valence'],
+    #              arousal=row['arousal'],
+    #              submissions=miner.query(row['song_name'], row['artist_name']))),
+    #              out, indent=4, ensure_ascii=False)
+#
 
 def minertype(istr: str):
     istr = istr.strip().lower()
@@ -60,18 +65,9 @@ def minertype(istr: str):
 
 
 def parseargs() -> argparse.Namespace:
-    parser = argparse.ArgumentParser(description="A Reddit bot for gathering social media comments \
-        connected to songs. A part of the Music Emotion Prediction project @ OSU-Cascades")
-    parser.add_argument('-i', dest='input', required=True, help='Input file. Should be a csv list of songs, \
-        containing artist_name and song_title, as well as valence and arousal values.')
-    parser.add_argument('-c', dest='config', required=True,
-                        help='Config file for PRAW.')
-    parser.add_argument('--dataset', dest='dataset', type=str, required=True,
-                        help='The name of the dataset.')
-    parser.add_argument('-o', dest='output', required=True,
-                        help='Destination folder for output files. Must be a directory.')
-    parser.add_argument('--search_depth', dest='search_depth', default=10, type=int,
-                        help='How many posts the reddit bot should scrape comments from')
-    parser.add_argument('-t', '--type', dest='bot_type', required=True, type=minertype, default=RedditBot,
-                        help='Specify which platform to perform queries on. Options include youtube, reddit.')
+    parser = argparse.ArgumentParser(description="A data inject tool for the music semantic discourse dataset.")
+    parser.add_argument('--dataset', dest='dataset', type=str, required=False,
+                        help='The name of the dataset to query songs from. Uses all songs by default.')
+    parser.add_argument('-t', '--type', dest='bot_type', required=True, type=minertype, 
+                        help='Specify which platform to query <reddit, youtube, twitter.')
     return parser.parse_args()
