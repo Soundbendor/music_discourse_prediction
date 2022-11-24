439e14a909a6c4a52a056577b0e249459c484e5b
Aidan-B1409 <aidanb1049@gmail.com>
2022-11-23 21:01:47 -0800

    Refactoring of yt module

--- src/data_mining/commentminer.py
+++ src/data_mining/commentminer.py
@@ -1,18 +1,19 @@
-import requests
 import tweepy
+import requests
 import praw
 
 from abc import abstractmethod
-from typing import List, Callable, TypeVar
-from time import sleep
+from typing import List, Callable, TypeVar, Union
 from langdetect import detect_langs
 from langdetect.lang_detect_exception import LangDetectException
 from langdetect.language import Language
-from pymongo.database import Database
 from bson.objectid import ObjectId
+from googleapiclient.errors import HttpError
+from database.driver import Driver
 from googleapiclient.discovery import Resource as YoutubeResource
 
 Client = TypeVar("Client", tweepy.Client, praw.Reddit, YoutubeResource)
+Error = Union[requests.exceptions.ConnectionError, HttpError]
 
 
 class CommentMiner:
@@ -27,15 +28,13 @@ class CommentMiner:
             artist_name.replace('"', ""), song_name.replace('"', "")
         )
 
-    def persist(self, func: Callable[[], List], retries: int = 3) -> List:
-        conn = 0
-        while conn <= retries:
+    def _persist(self, func: Callable, exceptions: tuple, retries: int = 3):
+        for i in range(0, retries):
             try:
                 return func()
-            except requests.exceptions.ConnectionError:
-                sleep(5)
-                print(f"Connection error! Retry #{conn}")
-                retries += 1
+            #  except requests.exceptions.ConnectionError:
+            except exceptions as e:
+                self._handler(e)
         exit()
 
     @abstractmethod
@@ -43,9 +42,13 @@ class CommentMiner:
         pass
 
     @abstractmethod
-    def fetch_comments(self, db: Database, song: dict) -> List[ObjectId]:
+    def fetch_comments(self, db: Driver, song: dict) -> List[ObjectId]:
         pass
 
     @abstractmethod
     def _authenticate(self) -> Client:
+        pass
+
+    @abstractmethod
+    def _handler(self, e: Error) -> None:
         pass
