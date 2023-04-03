Fix dataset scaling

src/models/bert_emotion_predictor.py
@@ -32,7 +32,8 @@ def parseargs() -> argparse.Namespace:
         "--input_dir",
         dest="input",
         type=str,
-        help="Path to the directory storing the JSON files for social media data.",
+        required=False,
+        help="Optional. Path to the directory storing the JSON files for social media data.",
     )
     parser.add_argument(
         "--source",
@@ -54,18 +55,11 @@ def parseargs() -> argparse.Namespace:
         help="Path to saved model state, if model doesn't exist at path, creates a new checkpoint.",
     )
     parser.add_argument(
-        "--num_epoch", type=int, default=50, dest="num_epoch", help="Number of epochs to train the model with"
+        "--num_epoch", type=int, default=5, dest="num_epoch", help="Number of epochs to train the model with"
     )
     parser.add_argument("--model_name", type=str, default="distilbert-base-cased", dest="model_name")
     parser.add_argument("--intersection", type=bool, default=False, dest="intersection")
     return parser.parse_args()
-
-
-def load_weights(model: tf.keras.Model, path: str):
-    try:
-        model.load_weights(path)
-    except Exception:
-        print("Model checkpoint invalid. Opening new model.")
 
 
 def get_num_gpus() -> int:
@@ -81,7 +75,6 @@ def get_songs(args: argparse.Namespace):
     if args.input:
         return pd.read_csv(args.input)
     db_con = Driver("mdp")
-
     df = pd.concat([db_con.get_discourse(ds_name=args.dataset, source_type=x) for x in args.sources], axis=0)
 
     if args.intersection:
@@ -92,8 +85,8 @@ def get_songs(args: argparse.Namespace):
 
 def main():
     args = parseargs()
-    df = get_songs(args)
-    print(df)
+    song_df = get_songs(args)
+    print(song_df)
 
     # Load API tokens from .env
     load_dotenv()
@@ -102,25 +95,21 @@ def main():
     neptune_runtime = neptune.init(project=os.getenv("NEPTUNE_PROJECT_ID"), api_token=os.getenv("NEPTUNE_API_TOKEN"))
     callbacks = [
         NeptuneCallback(run=neptune_runtime, base_namespace="metrics"),
-        ModelCheckpoint(args.model, monitor="loss", save_weights_only=True, verbose=1, save_best_only=True, mode="min"),
     ]
 
-    # Load our data from JSONs and randomize the dataset
-    # We shuffle here because tensorflow does not currently support dataset shuffling
-    song_df = get_songs(args)
+    # PREPROCESSING PIPELINE GOES HERE
 
     ds = DiscourseDataSet(song_df, t_prop=0.15)
 
     with tf.distribute.MultiWorkerMirroredStrategy().scope():
         model = create_model(args.model_name)
-        load_weights(model, args.model)
         print(model.summary())
 
         model.fit(
             x=generate_embeddings(ds.X_train, SEQ_LEN, args.model_name),
             y=ds.y_train,
             verbose=1,
-            batch_size=(BATCH_SIZE * get_num_gpus()),
+            batch_size=(BATCH_SIZE),
             validation_data=(generate_embeddings(ds.X_val, SEQ_LEN, args.model_name), ds.y_val),
             callbacks=callbacks,
             epochs=args.num_epoch,
@@ -129,7 +118,7 @@ def main():
         print("\n\nTesting...")
         y_pred = model.predict(
             x=generate_embeddings(ds.X_test, SEQ_LEN, args.model_name),
-            batch_size=(BATCH_SIZE * get_num_gpus()),
+            batch_size=(BATCH_SIZE),
             verbose=1,
             callbacks=callbacks,
         )

src/models/discourse_dataset.py
@@ -4,96 +4,108 @@ import pandas as pd
 import numpy as np
 import re
 
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import MinMaxScaler
-from transformers import DistilBertTokenizerFast, RobertaTokenizerFast
+from sklearn.preprocessing import MinMaxScaler, StandardScaler
+from transformers import DistilBertTokenizerFast, RobertaTokenizerFast, XLNetTokenizerFast
 
 RAND_SEED = 128
 
 
 def _tokenize(comments: pd.Series, tokenizer, seq_len: int) -> transformers.BatchEncoding:
-    return tokenizer(list(comments),
-                     add_special_tokens=True,
-                     return_attention_mask=True,
-                     return_token_type_ids=False,
-                     max_length=seq_len,
-                     padding='max_length',
-                     truncation=True,
-                     return_tensors='tf')
+    return tokenizer(
+        list(comments),
+        add_special_tokens=True,
+        return_attention_mask=True,
+        return_token_type_ids=False,
+        max_length=seq_len,
+        padding="max_length",
+        truncation=True,
+        return_tensors="tf",
+    )
 
 
 def generate_embeddings(df: pd.DataFrame, seq_len: int, model: str) -> dict:
     tokenizers = {
-        'distilbert-base-uncased': DistilBertTokenizerFast.from_pretrained('distilbert-base-uncased',
-                                                                           do_lower_case=True,
-                                                                           add_special_tokens=True,
-                                                                           max_length=seq_len,
-                                                                           padding='max_length',
-                                                                           truncate=True,
-                                                                           padding_side='right'),
-        'roberta-base': RobertaTokenizerFast.from_pretrained('roberta-base',
-                                                             do_lower_case=True,
-                                                             add_special_tokens=True,
-                                                             max_length=seq_len,
-                                                             padding='max_length',
-                                                             truncate=True,
-                                                             padding_side='right')
+        "distilbert-base-uncased": DistilBertTokenizerFast.from_pretrained(
+            "distilbert-base-uncased",
+            do_lower_case=True,
+            add_special_tokens=True,
+            max_length=seq_len,
+            padding="max_length",
+            truncate=True,
+            padding_side="right",
+        ),
+        "roberta-base": RobertaTokenizerFast.from_pretrained(
+            "roberta-base",
+            do_lower_case=True,
+            add_special_tokens=True,
+            max_length=seq_len,
+            padding="max_length",
+            truncate=True,
+            padding_side="right",
+        ),
+        "xlnet-base-cased": XLNetTokenizerFast.from_pretrained(
+            "xlnet-base-cased", max_length=seq_len, padding="max_length", truncate=True, padding_side="right"
+        ),
     }
 
     tokenizer = tokenizers[model]
-    encodings = _tokenize(df['body'], tokenizer, seq_len)
-    return {'input_token': encodings['input_ids'],
-            'masked_token': encodings['attention_mask']}
+    encodings = _tokenize(df["body"], tokenizer, seq_len)
+    return {"input_token": encodings["input_ids"], "masked_token": encodings["attention_mask"]}
 
 
 class DiscourseDataSet:
     def __init__(self, df: pd.DataFrame, t_prop: float):
-        self.df = self._clean_str(df.dropna(how='any', subset=['body']))
+        self.df = self._clean_str(df.dropna(how="any", subset=["body"]))
         # TODO - introduce validation subset
-        self.X_train,\
-            self.X_val,\
-            self.X_test,\
-            self.y_train,\
-            self.y_val,\
-            self.y_test = self._split_data(self.df, test_size=t_prop)
+        self.X_train, self.X_val, self.X_test, self.y_train, self.y_val, self.y_test = self._split_data(
+            self.df, test_size=t_prop
+        )
 
     # NOTE - ONLY cleans comment bodies. Adapt to post titles if needed.
     def _clean_str(self, df: pd.DataFrame):
-        rx = re.compile(r'(?:<.*?>)|(?:http\S+)')
-        df['body'] = df['body'].apply(lambda x: rx.sub('', x))
+        rx = re.compile(r"(?:<.*?>)|(?:http\S+)")
+        df["body"] = df["body"].apply(lambda x: rx.sub("", x))
         return df
 
     def _split_data(self, df: pd.DataFrame, test_size):
         np.random.seed(RAND_SEED)
         holdout_train_split = self._split_songs(df, test_size)
-        test_validate_split = self._split_songs(
-            holdout_train_split['Holdout'], 0.5)
-        print(holdout_train_split['Include'].shape)
-        print(holdout_train_split['Holdout'].shape)
-        print(test_validate_split['Include'].shape)
-        print(test_validate_split['Holdout'].shape)
+        test_validate_split = self._split_songs(holdout_train_split["Holdout"], 0.5)
 
-        return self._features(holdout_train_split['Include']),\
-            self._features(test_validate_split['Include']),\
-            self._features(test_validate_split['Holdout']),\
-            self._convert_labels(holdout_train_split['Include']),\
-            self._convert_labels(test_validate_split['Include']),\
-            self._convert_labels(test_validate_split['Holdout'])
+        y_train, y_val, y_test = self._convert_labels(
+            holdout_train_split["Include"], test_validate_split["Include"], test_validate_split["Holdout"]
+        )
+
+        return (
+            self._features(holdout_train_split["Include"]),
+            self._features(test_validate_split["Include"]),
+            self._features(test_validate_split["Holdout"]),
+            y_train,
+            y_val,
+            y_test,
+        )
 
     def _split_songs(self, df: pd.DataFrame, test_size: int) -> dict:
-        ids = df['song_name'].unique()
-        holdout_indices = np.random.choice(
-            ids, size=int(len(ids) * test_size), replace=False)
-        holdout_df = df.loc[df['song_name'].isin(holdout_indices)]
-        include_df = df.loc[~df['song_name'].isin(holdout_indices)]
-        return {'Holdout': holdout_df,
-                'Include': include_df}
+        ids = df["song_name"].unique()
+        holdout_indices = np.random.choice(ids, size=int(len(ids) * test_size), replace=False)
+        holdout_df = df.loc[df["song_name"].isin(holdout_indices)]
+        include_df = df.loc[~df["song_name"].isin(holdout_indices)]
+        return {"Holdout": holdout_df, "Include": include_df}
 
-    def _features(self, a: pd.DataFrame) -> pd.DataFrame:
-        return a.drop(['valence', 'arousal'], axis=1)
+    def _features(self, x: pd.DataFrame) -> pd.DataFrame:
+        return x.drop(["valence", "arousal"], axis=1)  # type: ignore
 
-    def _convert_labels(self, a: pd.DataFrame):
-        a = a[['valence', 'arousal']]
-        scaler = MinMaxScaler(feature_range=(0, 1))
-        a = scaler.fit_transform(a)
-        return np.asarray(a).astype('float32')
+    def _convert_labels(
+        self, y_train: pd.DataFrame, y_val: pd.DataFrame, y_test: pd.DataFrame
+    ) -> Tuple[np.ndarray, np.ndarray, np.ndarray]:
+        scaler = StandardScaler()
+        y_train = scaler.fit_transform(self._get_labels(y_train))
+        y_val = scaler.transform(self._get_labels(y_val))
+        y_test = scaler.transform(self._get_labels(y_train))
+        return y_train, y_val, y_test
+
+    def _get_labels(self, x: pd.DataFrame) -> pd.DataFrame:
+        return x[["valence", "arousal"]]  # type: ignore
+
+    def _to_float32(self, x: np.ndarray) -> np.ndarray:
+        return np.asarray(x).astype("float32")  # type: ignore