9bc77bfd624d00029496f16bc8f99e3832ec653c
Aidan-B1409 <aidanb1049@gmail.com>
2023-04-11 16:39:40 -0700

    Push histogram code

--- scripts/seqlen.py
+++ scripts/seqlen.py
@@ -11,8 +11,8 @@ DATASET = ["deezer", "deam_new" "amg1608", "pmemo"]
 db_con = Driver("mdp")
 for source in ["Reddit", "Youtube", "Twitter"]:
     df = db_con.get_discourse(ds_name="amg1608", source_type=source)
-    df["body"].str.len().describe()
-    hist = sns.histplot(data=df["body"].str.len().clip(0, 1000), kde=True)
+    print(df["body"].str.len().describe())
+    hist = sns.histplot(data=df["body"].str.len().clip(0, 1024), kde=True, color="rocket")
     hist.set(xlabel="Comment Length", ylabel="Songs")
     fig = hist.get_figure()
     fig.savefig(f"{source}_dist.png")
