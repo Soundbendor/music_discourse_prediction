3ad02f393c796ac301a3b1339234d3694a9f42e0
Aidan-B1409 <aidanb1049@gmail.com>
2023-04-12 14:30:56 -0700

    Change histogram drawing

--- scripts/seqlen.py
+++ scripts/seqlen.py
@@ -15,15 +15,17 @@ def make_hist(src: Union[List[str], str]) -> None:
     df = db_con.get_discourse(ds_name=DATASET, source_type=src)
     print(df["body"].str.len().describe())
     hist = sns.histplot(data=df["body"].str.len().clip(0, 1024), kde=True)
-    hist.set(yscale="log")
+    return hist
 
 
 sns.color_palette("rocket", as_cmap=True)
 db_con = Driver("mdp")
+hist = None
 for source in SOURCES:
     hist = make_hist(source)
 
 # Bad stupid code design
+hist.set(yscale="log")
 hist.set(xlabel="Comment Length", ylabel="Songs")
 plt.legend(labels=SOURCES)
 fig = hist.get_figure()
