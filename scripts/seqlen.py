455177a349181a0f7a512fdfa42cf1c705a28287
Aidan-B1409 <aidanb1049@gmail.com>
2023-04-11 19:16:28 -0700

    Push histogram code

--- scripts/seqlen.py
+++ scripts/seqlen.py
@@ -1,23 +1,27 @@
+from typing import List, Union
+
 import seaborn as sns
 
 from database.driver import Driver
 
+SOURCES = ["Reddit", "Youtube", "Twitter"]
 DATASET = ["deezer", "deam_new" "amg1608", "pmemo"]
 # Averages for all individual sources and datasets
 
-# Cumulative histograms
 
-# Total final histogram
-db_con = Driver("mdp")
-for source in ["Reddit", "Youtube", "Twitter"]:
-    df = db_con.get_discourse(ds_name="amg1608", source_type=source)
+# Cumulative histograms
+def make_hist(src: Union[List[str], str]) -> None:
+    df = db_con.get_discourse(ds_name="amg1608", source_type=src)
     print(df["body"].str.len().describe())
-    hist = sns.histplot(data=df["body"].str.len().clip(0, 1024), kde=True, color="oranges")
+    hist = sns.histplot(data=df["body"].str.len().clip(0, 1024), kde=True)
     hist.set(xlabel="Comment Length", ylabel="Songs")
     fig = hist.get_figure()
-    fig.savefig(f"{source}_dist.png")
+    fig.savefig(f"{src}_dist.png")
 
 
-# All-source histogram
+sns.color_palette("rocket", as_cmap=True)
+db_con = Driver("mdp")
+for source in sources:
+    make_hist(source)
 
-# Summary statistics
+make_hist(sources)
