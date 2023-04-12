1c9ba2f78323020e02f7e89b3c1102410e3a72f8
Aidan-B1409 <aidanb1049@gmail.com>
2023-04-12 14:22:36 -0700

    Change histogram drawing

--- scripts/seqlen.py
+++ scripts/seqlen.py
@@ -1,5 +1,6 @@
 from typing import List, Union
 
+import matplotlib.pyplot as plt
 import seaborn as sns
 
 from database.driver import Driver
@@ -14,14 +15,19 @@ def make_hist(src: Union[List[str], str]) -> None:
     df = db_con.get_discourse(ds_name=DATASET, source_type=src)
     print(df["body"].str.len().describe())
     hist = sns.histplot(data=df["body"].str.len().clip(0, 1024), kde=True)
-    hist.set(xlabel="Comment Length", ylabel="Songs")
-    fig = hist.get_figure()
-    fig.savefig(f"{src}_dist.png")
+    hist.set(yscale="log")
 
 
 sns.color_palette("rocket", as_cmap=True)
 db_con = Driver("mdp")
 for source in SOURCES:
-    make_hist(source)
+    hist = make_hist(source)
 
-make_hist(SOURCES)
+# Bad stupid code design
+hist.set(xlabel="Comment Length", ylabel="Songs")
+plt.legend(labels=SOURCES)
+fig = hist.get_figure()
+fig.savefig(f"all_dist.png")
+
+
+# make_hist(SOURCES)
