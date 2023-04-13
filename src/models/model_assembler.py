09f561b79f39f095ee54e37efaf73954ef2e50ce
Aidan-B1409 <aidanb1049@gmail.com>
2023-04-12 20:02:45 -0700

    Freeze BERT

--- src/models/model_assembler.py
+++ src/models/model_assembler.py
@@ -10,13 +10,18 @@ def create_model(model_name: str) -> tf.keras.Model:
     config = AutoConfig.from_pretrained(model_name)
     print(model_name)
     db_seq = TFAutoModel.from_config(config)
+
+    for layer in db_seq.layers:
+        layer.trainable = False
+        for w in layer.weights:
+            w._trainable = False
+
     print(db_seq)
 
     input_ids = tf.keras.layers.Input(shape=(MAX_SEQ_LEN,), name="input_token", dtype="int32")
     input_masks_ids = tf.keras.layers.Input(shape=(MAX_SEQ_LEN,), name="masked_token", dtype="int32")
 
     output = db_seq(input_ids, input_masks_ids).last_hidden_state[:, 0, :]
-    # output = tf.keras.layers.Dense(768, activation='relu')(output)
     output = tf.keras.layers.Dense(MAX_SEQ_LEN, activation="relu")(output)
     output = tf.keras.layers.Dense(2, activation="linear")(output)
 
