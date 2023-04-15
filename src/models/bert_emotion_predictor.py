322ff4aa1e4200b68c16c00a7747c0e6c8413b99
Aidan-B1409 <aidanb1049@gmail.com>
2023-04-15 10:20:47 -0700

    Caching and logging

--- src/models/bert_emotion_predictor.py
+++ src/models/bert_emotion_predictor.py
@@ -1,7 +1,7 @@
 import argparse
 import os
 from datetime import datetime
-from typing import List
+from typing import List, Tuple
 
 import matplotlib.pyplot as plt
 import neptune.new as neptune
@@ -13,7 +13,7 @@ from neptune.new.integrations.tensorflow_keras import NeptuneCallback
 from scipy.stats import pearsonr
 
 from database.driver import Driver
-from visualization.visualizations import circumplex_model
+from visualization.visualizations import *
 
 from .discourse_dataset import DiscourseDataSet, generate_embeddings
 from .model_assembler import create_model
@@ -58,10 +58,6 @@ def get_num_gpus() -> int:
     return len(tf.config.list_physical_devices("GPU"))
 
 
-# Need support "save to disk" option
-# Handle intersection types, sometimes we only want songs that appear in all 3 datasets
-
-
 def get_songs(args: argparse.Namespace):
     # If input csv is provided, load it and return it.
     if args.input:
@@ -71,7 +67,9 @@ def get_songs(args: argparse.Namespace):
 
     if args.intersection:
         print(df["source"])
-        return df.groupby("_id").filter(lambda group: all([group["source"].eq(x).any() for x in args.sources]))
+        df = df.groupby("_id").filter(lambda group: all([group["source"].eq(x).any() for x in args.sources]))
+    print("Caching new dataframe...")
+    df.to_csv(f"cache_{args.dataset}_{' '.join(args.sources)}")
     return df
 
 
@@ -91,12 +89,16 @@ def main():
         tf.keras.callbacks.TensorBoard(log_dir=log_dir),
     ]
 
-    # PREPROCESSING PIPELINE GOES HERE
-
     ds = DiscourseDataSet(
         song_df, t_prop=0.15, length_threshold=args.length_threshold, score_threshold=args.score_threshold
     )
-    y_pred = run_experiment(ds, args, callbacks)
+
+    # Cache results CSV
+    y_pred, history = run_experiment(ds, args, callbacks)
+    print("\n\n\nKEYS")
+    print(history.keys())
+    pd.DataFrame(y_pred).to_csv(f"result_{args.dataset}_{datetime.now().strftime('%Y%m%d-%H%M%S')}")
+    neptune_runtime["aux/data"].upload(f"result_{args.dataset}_{datetime.now().strftime('%Y%m%d-%H%M%S')}")
 
     valence_corr = pearsonr(ds.y_test[:, 0], y_pred[:, 0])
     arr_corr = pearsonr(ds.y_test[:, 1], y_pred[:, 1])
@@ -104,16 +106,15 @@ def main():
     print(f"Pearson's Correlation (comment level) - Valence: {valence_corr}")
     print(f"Pearson's Correlation (comment level) - Arousal: {arr_corr}")
 
-    # TODO - sm_type is now a list, handle this case gracefully with a join
-    aggregate_predictions(ds.X_test, ds.y_test, y_pred, neptune_runtime, f"{' '.join(args.sources)}_{args.dataset}")
+    aggregate_predictions(ds.X_test, ds.y_test, y_pred, neptune_runtime, f"{args.dataset.upper()}")
 
 
-def run_experiment(ds: DiscourseDataSet, args: argparse.Namespace, callbacks: List) -> np.ndarray:
+def run_experiment(ds: DiscourseDataSet, args: argparse.Namespace, callbacks: List) -> Tuple[np.ndarray, dict]:
     with tf.distribute.MultiWorkerMirroredStrategy().scope():
         model = create_model(args.model_name)
         print(model.summary())
 
-        model.fit(
+        history = model.fit(
             x=generate_embeddings(ds.X_train, SEQ_LEN, args.model_name),
             y=ds.y_train,
             verbose=1,
@@ -130,7 +131,7 @@ def run_experiment(ds: DiscourseDataSet, args: argparse.Namespace, callbacks: Li
             verbose=1,
             callbacks=callbacks,
         )
-        return y_pred
+        return y_pred, history.history
 
 
 def aggregate_predictions(X: pd.DataFrame, y: np.ndarray, pred: np.ndarray, run: neptune.Run, fname: str):
@@ -152,22 +153,8 @@ def aggregate_predictions(X: pd.DataFrame, y: np.ndarray, pred: np.ndarray, run:
     print(f"Pearson's Correlation (song level) - Valence: {valence_corr}")
     print(f"Pearson's Correlation (song level) - Arousal: {arr_corr}")
 
-    scatterplot(results, "valence", "val_pred", "valence_scatter", "Valence", run)
-    scatterplot(results, "arousal", "aro_pred", "arousal_scatter", "Arousal", run)
+    scatterplot(results, "Valence - Actual", "Valence - Predicted", "valence_scatter", "Valence", run)
+    scatterplot(results, "Arousal - Actual", "Arousal - Predicted", "arousal_scatter", "Arousal", run)
 
-    # TODO - Update this parameter list or backport vis.py
     circumplex_model(results["val_pred"], results["aro_pred"], f"{fname} - Predicted", f"{fname}_predicted.png", run)
     circumplex_model(results["valence"], results["arousal"], f"{fname} - Actual", f"{fname}_actual.png", run)
-
-
-def scatterplot(df: pd.DataFrame, x_key: str, y_key: str, fname: str, title: str, run: neptune.Run) -> None:
-    fig = plt.figure()
-    plt.scatter(x=df[x_key], y=df[y_key], alpha=0.5, s=10)
-    m, b = np.polyfit(df[x_key], df[y_key], 1)
-    plt.plot(df[x_key], m * df[x_key] + b, "r:", alpha=0.2, linewidth=2)
-    fig.suptitle(title)
-    plt.xlabel(x_key)
-    plt.ylabel(y_key)
-    run[title].log(fig)
-    fig.savefig(fname)
-    plt.clf()
