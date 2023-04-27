6a94fe16214c72f2c466981b593fdbea41c330cd
Aidan-B1409 <aidanb1049@gmail.com>
2023-04-27 14:47:50 -0700

    Update histogram code

--- scripts/seqlen.py
+++ scripts/seqlen.py
@@ -12,11 +12,19 @@ DATASET = ["deam_new", "amg1608", "pmemo"]
 
 
 # Cumulative histograms
-def make_hist(src: Union[List[str], str]) -> None:
-    df = db_con.get_discourse(ds_name=DATASET, source_type=src)
-    print(df["body"].apply(lambda x: len(wordpunct_tokenize(x))).clip(0, 1024).describe())
+def make_word_hist(src: Union[List[str], str]) -> None:
+    df = db_con.get_discourse(ds_name=src, source_type=["Reddit", "Youtube"]
+    print(df["body"].apply(lambda x: len(wordpunct_tokenize(x))).describe())
     hist = sns.histplot(
-        data=df["body"].apply(lambda x: len(wordpunct_tokenize(x))).clip(0, 1024), kde=True, bins=range(0, 1025, 64)
+        data=df["body"].apply(lambda x: len(wordpunct_tokenize(x))), kde=True, bins=range(0, 1025, 64)
+    )
+    return hist
+
+def make_comment_hist(src: Union[List[str], str]) -> None:
+    df = db_con.get_discourse(ds_name=src, source_type=["Reddit", "Youtube"]
+    print(df["body"].apply(lambda x: len(wordpunct_tokenize(x))).describe())
+    hist = sns.histplot(
+        data=df["body"].apply(lambda x: len(wordpunct_tokenize(x))), kde=True, bins=range(0, 1025, 64)
     )
     return hist
 
@@ -24,15 +32,12 @@ def make_hist(src: Union[List[str], str]) -> None:
 sns.color_palette("rocket", as_cmap=True)
 db_con = Driver("mdp")
 hist = None
-for source in SOURCES:
-    hist = make_hist(source)
+for ds in DATASET:
+    hist = make_word_hist(source)
 
 # Bad stupid code design
-hist.set(yscale="log")
-hist.set(xlabel="Comment Length", ylabel="Songs")
-plt.legend(labels=SOURCES)
+hist.set(xscale="log")
+hist.set(xlabel="# Words", ylabel="Songs")
+plt.legend(labels=DATASET)
 fig = hist.get_figure()
-fig.savefig(f"all_dist.png")
-
-
-# make_hist(SOURCES)
+fig.savefig(f"all_dist_word.png")
