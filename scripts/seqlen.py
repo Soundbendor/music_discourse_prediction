bee5894221c894b475184d423962c6edf053d069
Aidan-B1409 <aidanb1049@gmail.com>
2023-04-27 19:26:38 -0700

    Refactor

--- scripts/seqlen.py
+++ scripts/seqlen.py
@@ -9,6 +9,7 @@ from database.driver import Driver
 
 SOURCES = ["Reddit", "Youtube", "Twitter"]
 DATASET = ["deezer", "deam_new", "amg1608", "pmemo"]
+STAGE_NAME = ["Deezer", "DEAM", "AMG1608", "PMEmo"]
 # Averages for all individual sources and datasets
 
 
@@ -39,15 +40,38 @@ def make_comment_hist(src: Union[List[str], str]) -> None:
     return hist
 
 
+def get_n_words(df: pd.DataFrame) -> pd.Series:
+    df["body"] = df["body"].astype(str).apply(lambda x: len(wordpunct_tokenize(x)))
+    return df.groupby(["song_name"])["body"].sum()
+
+
+def make_hist(df: pd.DataFrame) -> None:
+    return sns.histplot(data=df, kde=True, bins=32, log_scale=True)
+
+
 sns.color_palette("rocket", as_cmap=True)
 db_con = Driver("mdp")
-hist = None
-for ds in DATASET:
-    hist = make_word_hist(ds)
+
+df = pd.concat(
+    axis=0,
+    ignore_index=True,
+    objs=list(
+        map(
+            lambda x: pd.DataFrame.from_dict({"value": x[0], "name": x[1]}),
+            zip(
+                map(
+                    get_n_words, [db_con.get_discourse(ds_name=ds, source_type=["Reddit", "Youtube"]) for ds in DATASET]
+                ),
+                STAGE_NAME,
+            ),
+        )
+    ),
+)
+hist = make_hist(df)
 
 # Bad stupid code design
 # hist.set(xscale="log")
 hist.set(xlabel="# Words", ylabel="Songs")
-plt.legend(labels=["Deezer", "DEAM", "AMG1608", "PMEmo"])
+plt.legend(labels=STAGE_NAME)
 fig = hist.get_figure()
 fig.savefig(f"all_dist_word.png")
