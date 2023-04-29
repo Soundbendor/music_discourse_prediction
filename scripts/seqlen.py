dd84f7cbae7e69156213384947b82062d72300e6
Aidan-B1409 <aidanb1049@gmail.com>
2023-04-28 21:36:57 -0700

    Clip histograms

--- scripts/seqlen.py
+++ scripts/seqlen.py
@@ -16,33 +16,6 @@ STAGE_NAME = ["Deezer", "DEAM", "AMG1608", "PMEmo"]
 # STAGE_NAME = ["DEAM", "AMG1608", "PMEmo"]
 
 
-# Cumulative histograms
-def make_word_per_comment_hist(src: Union[List[str], str]) -> None:
-    df = db_con.get_discourse(ds_name=src, source_type=["Reddit", "Youtube"])
-    print(df["body"].apply(lambda x: len(wordpunct_tokenize(x))).describe())
-    hist = sns.histplot(data=df["body"].apply(lambda x: len(wordpunct_tokenize(x))), kde=True)
-    return hist
-
-
-def make_word_hist(src: Union[List[str], str]) -> None:
-    df = db_con.get_discourse(ds_name=src, source_type=["Reddit", "Youtube"])
-    # Currently counts # words per comment
-    # df = df.groupby(["song_name"])["body"].apply(lambda x: len(wordpunct_tokenize(x))).sum()
-    df["body"] = df["body"].astype(str).apply(lambda x: len(wordpunct_tokenize(x)))
-    df = df.groupby(["song_name"])["body"].sum()
-    print(df.describe())
-    hist = sns.histplot(data=df, kde=True, bins=32, log_scale=True)
-    return hist
-
-
-def make_comment_hist(src: Union[List[str], str]) -> None:
-    df = db_con.get_discourse(ds_name=src, source_type=["Reddit", "Youtube"])
-    print(df.describe())
-    df = df.groupby(["song_name"])["body"].sum()
-    hist = sns.histplot(data=df, kde=True)
-    return hist
-
-
 def get_n_words(df: pd.DataFrame) -> pd.Series:
     df["body"] = df["body"].astype(str).apply(lambda x: len(wordpunct_tokenize(x)))
     return df.groupby(["song_name"])["body"].sum()
@@ -53,7 +26,8 @@ def get_n_comments(df: pd.DataFrame) -> pd.Series:
 
 
 def make_hist(df: pd.DataFrame) -> None:
-    return sns.histplot(data=df, x="value", hue="name", kde=True, bins=24, log_scale=True)
+    sns.set(font_scale=1.25)
+    return sns.histplot(data=df, x="value", hue="name", kde=True, bins=24, log_scale=True, binrange=(0, 500))
 
 
 def get_song_word_ratio(df: pd.DataFrame) -> pd.Series:
@@ -94,9 +68,6 @@ df = df.dropna()
 df = df[np.isfinite(df["value"])]
 hist = make_hist(df)
 
-# Bad stupid code design
-# hist.set(xscale="log")
 hist.set(xlabel="# Words", ylabel="Comments")
-# plt.legend()
 fig = hist.get_figure()
 fig.savefig(f"word_comment_ratio.png")
